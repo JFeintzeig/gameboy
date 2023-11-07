24c1e5ed43ed80a2c503d4232ba95c69b6082132
JFeintzeig <JFeintzeig@users.noreply.github.com>
2023-11-07 17:45:32 -0500

    much stuff

--- internal/cpu/cpu.go
+++ internal/cpu/cpu.go
@@ -40,24 +40,159 @@ func (gb *Motherboard) LoadBootROM() {
 }
 
 func (gb *Motherboard) Execute() {
-  for i := 0; i <= 100; i++ {
-    fmt.Printf("%x: %x\n",gb.cpu.pc, gb.memory[gb.cpu.pc])
-    gb.cpu.pc++
+  // TODO: execute micro-ops and make logic to choose when to advance
+  // to next byte to decode...
+  // TODO: timing
+  for i := 0; i <= 10; i++ {
+    //fmt.Printf("%x: %x, %b\n",gb.cpu.PC.read(), gb.memory[gb.cpu.PC.read()], gb.memory[gb.cpu.PC.read()])
+
+    // TODO: tricky thing i need to figure out. this function
+    // (a) decodes a new opcode
+    // (b) adds its micro-instructions to the queue
+    // (c) runs 1 micro instruction at a time for each clock cycle
+    // (c) requires persisting (a). we only want to do (b) once for each
+    // opcode, but the only way i know to persist (a) is to do it for
+    // every (c). and then i _think_ gameboy timing can do a new (a) and (b)
+    // while one (c) runs for the previous opcode, further complicating matters.
+    // how to make this logic?
+    // AND the first pass is hard -- i need oc but i dont want to increment
+    // PC right away. maybe i can prefill ExecutionQueue with a dummy
+    // instruction? janky!! maybe i can set currentOc as a value on cpu and
+    // track state somehow? dont want it to be too complicated...
+
+    fmt.Printf("%x %d\n", gb.cpu.PC.read(), gb.cpu.ExecutionQueue.Length())
+    oc := ByteToOpcode(gb.memory[gb.cpu.PC.read()])
+    fmt.Printf("Decoding %x X:%b Y:%b Z:%b P:%b Q:%b\n", oc.Full, oc.X, oc.Y, oc.Z, oc.P, oc.Q)
+
+    fmt.Printf("%x %d\n", gb.cpu.PC.read(), gb.cpu.ExecutionQueue.Length())
+    if gb.cpu.ExecutionQueue.Length() < 1 {
+      // TODO: where to do this? i need PC pointing at
+      // currently-running opcode if we are still executing
+      // its microops, b/c microops depend on op
+      fmt.Println("gonna increment PC")
+      gb.cpu.PC.inc()
+      oc = ByteToOpcode(gb.memory[gb.cpu.PC.read()])
+      switch {
+      case (oc.X == 0) && (oc.Z == 1) && (oc.Q == 0):
+        fmt.Println("found it")
+        inst := gb.cpu.InstructionMap["X0Z1Q0"]
+        inst.AddOpsToQueue(&gb.cpu)
+      default:
+        fmt.Println("not implemented")
+      }
+    }
+
+    // i want to run this whether i am adding ops to the queue or not
+    // but we need _something_ in the queue to do this pop, otherwise
+    // it fails at runtime
+    // THIS IS BUGGY! LOOK AT THE PRINTS, I Step op before i should
+    // and weird stuff is happening and i dont know why
+    fmt.Printf("%x %d\n", gb.cpu.PC.read(), gb.cpu.ExecutionQueue.Length())
+    if gb.cpu.ExecutionQueue.Length() > 0 {
+      microop := gb.cpu.ExecutionQueue.Pop()
+      fmt.Printf("Executing %x\n", oc.Full)
+      (*microop)(&gb.cpu, &oc)
+    }
   }
 }
 
+type Register8 struct {
+  value uint8
+  name string
+}
+
+func (reg *Register8) read() uint8 {
+  return reg.value
+}
+
+func (reg *Register8) write(value uint8) {
+  reg.value = value
+}
+
+func (reg *Register8) inc() {
+  reg.value += 1
+}
+
+func (reg *Register8) dec() {
+  reg.value -= 1
+}
+
+type Register16 struct {
+  lo Register8
+  hi Register8
+}
+
+func (reg *Register16) read() uint16 {
+  return (uint16(reg.hi.value) << 8) | uint16(reg.lo.value)
+}
+
+func (reg *Register16) write(value uint16) {
+  reg.writeLo(value)
+  reg.writeHi(value)
+}
+
+func (reg *Register16) writeHi(value uint16) {
+  reg.hi.write(uint8(value >> 8))
+}
+
+func (reg *Register16) writeLo(value uint16) {
+  reg.lo.write(uint8(value&0xff))
+}
+
+func (reg *Register16) inc() {
+  reg.write(reg.read()+1)
+}
+
+func (reg *Register16) dec() {
+  reg.write(reg.read()-1)
+}
+
+func (reg *Register16) getName() string {
+  return reg.hi.name + reg.lo.name
+}
+
+type Fifo[T any] struct {
+  values []T
+}
+
+func (fifo *Fifo[T]) Push(val T) {
+  fifo.values = append(fifo.values, val)
+}
+
+func (fifo *Fifo[T]) Pop() T {
+  x, a := fifo.values[0], fifo.values[1:]
+  fifo.values = a
+  return x
+}
+
+func (fifo *Fifo[T]) Length() int {
+  return len(fifo.values)
+}
+
 type Cpu struct {
-  regA uint8
-  regF uint8
-  regB uint8
-  regC uint8
-  regD uint8
-  regE uint8
-  regH uint8
-  regL uint8
-  pc uint16
-  sp uint16
+  A Register8
+  F Register8
+  B Register8
+  C Register8
+  D Register8
+  E Register8
+  H Register8
+  L Register8
+
+  AF Register16
+  BC Register16
+  DE Register16
+  HL Register16
+
+  PC Register16
+  SP Register16
   clockSpeed uint64
+
+  rpTable []Register16
+
+  InstructionMap map[string]Instruction
+
+  ExecutionQueue Fifo[*func(*Cpu, *Opcode)]
 }
 
 func (cpu *Cpu) setFlagZ() {
@@ -72,12 +207,23 @@ func (cpu *Cpu) setFlagH() {
 func (cpu *Cpu) setFlagC() {
 }
 
+// TODO: implement ReadNN(), ReadN(), ReadD() on Cpu
+func (cpu *Cpu) ReadNN() uint16 {
+  return 0
+}
+
 type Ppu struct {
   screen [160*144]uint8
 }
 
 func NewGameBoy(romFilePath *string) *Motherboard {
-  cpu := Cpu{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, CLOCK_SPEED}
+  cpu := Cpu{}
+ 
+  cpu.clockSpeed = CLOCK_SPEED
+  cpu.rpTable = []Register16{cpu.BC, cpu.DE, cpu.HL, cpu.SP}
+
+  cpu.InstructionMap = MakeInstructionMap()
+
   ppu := Ppu{[160*144]uint8{}}
   gb := Motherboard{cpu, [8192]byte{}, [8192]byte{}, ppu, romFilePath}
   gb.LoadBootROM()
---
+++ internal/cpu/instructions.go
@@ -0,0 +1,75 @@
+package cpu
+
+// Opcode is the parsed octal representation of a byte
+// https://gb-archive.github.io/salvage/decoding_gbz80_opcodes/Decoding%20Gamboy%20Z80%20Opcodes.html
+type Opcode struct {
+  Full uint8
+  X uint8
+  Y uint8
+  Z uint8
+  P uint8
+  Q uint8
+}
+
+// TODO: document this logic and these magic numbers
+func ByteToOpcode(oneByte uint8) Opcode {
+  op := Opcode{
+    Full: oneByte,
+    X: uint8(0b11000000 & oneByte) >> 6,
+    Y: uint8(0b111000 & oneByte) >> 3,
+    Z: uint8(0b111 & oneByte),
+    P: uint8(0b110000 & oneByte) >> 4,
+    Q: (uint8(0b1000 & oneByte) >> 3) % 2,
+  }
+  return op
+}
+
+// Instruction contains the actual execution logic for
+// one or more instructions
+type Instruction struct {
+  name string
+  nBytes uint8
+  operations []func(*Cpu, *Opcode)
+}
+
+func (inst *Instruction) AddOpsToQueue(cpu *Cpu) {
+  for _, op := range inst.operations {
+    cpu.ExecutionQueue.Push(&op)
+  }
+}
+
+// then the CPUs execution cycle should deal with timing and
+// execute one microinstruction at a time
+// Question: ...but then how does CPU call AddOpsToQueue() to
+// populate the queue? and how deal with timing for the fetch
+// instruction step? maybe after every instruction, a Fetch()
+// function is added to the end of the queue to get the next opcode?
+// then i guess a long case match statement to route incoming opcodes
+// to instructions? wish the routing was easier...maybe if i give them
+// all my own names then i could put them in a hashmap or something, maybe
+// just like X1Y7Z2?
+
+func MakeInstructionMap() map[string]Instruction {
+  op1 := func (cpu *Cpu, op *Opcode) {
+    nn := cpu.ReadNN()
+    cpu.rpTable[op.P].writeLo(nn)
+  }
+  
+  op2 := func (cpu *Cpu, op *Opcode) {
+    nn := cpu.ReadNN()
+    cpu.rpTable[op.P].writeHi(nn)
+  }
+
+  instructionMap := make(map[string]Instruction)
+
+  // the keys in this map are just my internal
+  // names based on the X/Y/Z/P/Q's we need to
+  // match on, since its a many -> one mapping
+  // of opcodes to actual exection instructions.
+  instructionMap ["X0Z1Q0"] = Instruction{
+    "LD rp[p] nn",
+    3,
+    []func(*Cpu, *Opcode){op1, op2},
+  }
+  return instructionMap
+}
