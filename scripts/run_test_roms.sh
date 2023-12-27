fe6c445f1d1c5a713370ae340299916b66b49b48
JFeintzeig <JFeintzeig@users.noreply.github.com>
2023-12-26 19:23:03 -0500

    minor improve test rom script

--- scripts/run_test_roms.sh
+++ scripts/run_test_roms.sh
@@ -5,11 +5,11 @@ printout() {
 
 ROMDIR=$1
 
-cd ~/projects/2023/gameboy_resources/gameboy-doctor
+#cd ~/projects/2023/gameboy_resources/gameboy-doctor
 IFS=$'\n'
 for file in `ls ${ROMDIR}/*.gb`; do
   echo "***CPU INSTR TEST: `basename $file`"
-  ~/projects/2023/gameboy/app -file $file
+  ~/projects/2023/gameboy/app -file $file -bootrom
 done
 
-cd -
+#cd -
